433f31939df46f81da722cc39a7aed480f48ddaa
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-03 00:50:44 +0300

    added context in app and more routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import Layout from "./components/layouts/Layout"
 import Home from "./components/Home"
 import Register from "./components/Register"
 import Login from "./components/Login"
+import { AuthProvider } from "./context/authContext"
+import Products from "./components/Products"
+import Logout from "./components/Logout"
 
 function App() {
 
 
     return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-            </Route>
-        </Routes>
+        <AuthProvider>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path='/products' element={<Products />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Route>
+            </Routes>
+        </AuthProvider>
+
     )
 }
 
