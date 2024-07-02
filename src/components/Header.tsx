15aac05a75e8129ed9ad4b126df82cd7011d6665
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-03 00:49:10 +0300

    added dynamic navigation if user is logged

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
+import { useAuth } from '@/context/authContext'
+
+
 const Header = () => {
+    const { user } = useAuth()
     return (
         <header className='py-2 px-4 flex justify-around bg-slate-800'>
             <div className='w-24'>
@@ -13,13 +17,26 @@ const Header = () => {
                     <li >
                         <Link to='/products'>Products</Link>
                     </li>
-                    <li>
-                        <Link to='/register'>Register</Link>
-                    </li>
-                    <li>
-                        <Link to='/login'>Login</Link>
-                    </li>
 
+                    {user?.email ? (
+                        <>
+                            <li>
+                                <p>Welcome {user.displayName}</p>
+                            </li>
+                            <li>
+                                <Link to='/logout'>Logout</Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to='/register'>Register</Link>
+                            </li>
+                            <li>
+                                <Link to='/login'>Login</Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </header>
