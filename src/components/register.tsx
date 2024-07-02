b1df5b5e82d621cf65ce9b48a874f36cf221881b
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-03 00:48:49 +0300

    changed register to work with auth context

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -3,15 +3,16 @@ import { FormLabel, } from '@/ui/form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/firebase';
 import FormInput from './FormInput';
+import { useAuth } from '@/context/authContext';
+
 
 
 const formSchema = z.object({
     email: z.string().trim().email({
         message: 'Enter a valid email.'
     }),
+    displayName: z.string({ message: 'Enter display name' }).trim(),
     password: z.string().trim().min(6, {
         message: 'Password must be at least 6 characters.'
     }),
@@ -24,11 +25,13 @@ const formSchema = z.object({
 })
 
 const Register = () => {
+    const { register } = useAuth();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             email: '',
+            displayName: '',
             password: '',
             repass: ''
         }
@@ -36,12 +39,14 @@ const Register = () => {
 
     function onSubmit(values: z.infer<typeof formSchema>) {
 
-        createUserWithEmailAndPassword(auth, values.email, values.password)
-            .then(user => console.log(user)) /// TODOO SET USER IN CONTEXT 
-            .catch(err => {
-                const error = err.message == 'Firebase: Error (auth/email-already-in-use).' ? 'Email is already in use ' : 'Register Error'
+        try {
+            register(values.email, values.password, values.displayName)
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                const error = err.message == 'Firebase: Error (auth/email-already-in-use).' ? 'Email is already in use!' : 'Register Error'
                 form.setError('email', { type: 'manual', message: error });
-            })
+            }
+        }
     }
 
     return (
@@ -49,6 +54,7 @@ const Register = () => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="w-auto flex flex-col items-center gap-5 ">
                 <FormLabel>Register</FormLabel>
                 <FormInput name='email' type='text' />
+                <FormInput name='displayName' type='text' />
                 <FormInput name='password' type='password' />
                 <FormInput name='repass' type='password' />
                 <Button className='' type="submit">Register</Button>
