fc773153a89124832045e67d6adddc662893cb12
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-06 16:40:46 +0300

    implemented upload logic and loader

--- src/components/createForm/SellForm.tsx
+++ src/components/createForm/SellForm.tsx
@@ -1,21 +1,24 @@
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { FormProvider, useForm } from "react-hook-form"
+import { addDoc, collection, } from "firebase/firestore"
+import { firestore, storageRef } from "../../firebase"
+import { getDownloadURL, uploadBytes } from "firebase/storage"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 import { Button } from "@/ui/button"
 import { FormLabel } from "@/ui/form"
-import { useState } from "react"
 
-
+import { useAuth } from "@/context/authContext"
+import { isUserDefined } from "@/utils/typeGuards"
 import FormInput from "../common/FormInput"
 import FormSelect from "./FormSelect"
 import ImageUpload from "./ImageUpload"
 import FormTextarea from "./FormTextarea"
 import FormContacts from "./FormContacts"
-import { useAuth } from "@/context/authContext"
-import { isUserDefined } from "@/utils/typeGuards"
-import { addDoc, collection, doc, setDoc } from "firebase/firestore"
-import { firestore } from "../../firebase"
+import Loader from "../common/Loader"
+import generateRandomString from "@/utils/randomStringGenerator"
 
 
 const formSchema = z.object({
@@ -23,10 +26,15 @@ const formSchema = z.object({
     category: z.string().trim(),
     description: z.string().trim(),
     phoneNumber: z.string().trim(),
-    email: z.string().trim()
+    email: z.string().trim(),
+    displayName: z.string().trim()
 })
 
 const SellForm = () => {
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(false)
+
+
     const [images, setImages] = useState<File[]>([])
     const { user } = useAuth()
     const form = useForm<z.infer<typeof formSchema>>({
@@ -36,7 +44,8 @@ const SellForm = () => {
             category: '',
             description: '',
             phoneNumber: '',
-            email: '',
+            email: isUserDefined(user) ? user.email! : '',
+            displayName: isUserDefined(user) ? user.displayName! : ''
         }
     });
 
@@ -45,33 +54,40 @@ const SellForm = () => {
     }
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-        console.log(images)
-        // await addDoc(collection(firestore, 'products'), {
-        //     values
-        // })
-        // await setDoc(doc(firestore, 'products'), {
-        //     values
-        // })
+        setLoading(true);
+        Promise.all(images.map(image => uploadBytes(storageRef(generateRandomString(25)), image)))
+            .then(snapshot => {
+                Promise.all(snapshot.map(s => getDownloadURL(s.ref)))
+                    .then(async (images) => {
+                        await addDoc(collection(firestore, 'products'), { ...values, images })
+                        setLoading(false);
+                        navigate('/products')
+                    })
+            })
     }
 
     return (
-
-        <div className="w-full flex flex-col items-center" >
-            <FormProvider {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col flex-initial w-full max-w-[1000px] p-4 gap-6  text-slate-800">
-                    <FormLabel className=" text-slate-800 text-2xl">Какво продаваш</FormLabel>
-                    <div className="flex flex-col gap-6 bg-slate-200 p-4">
-                        <FormInput name='product' type='text' placeholder='Име на обявата' className="bg-white" />
-                        <FormSelect name='category' placeholder='Избери категория' />
-                    </div>
-                    <ImageUpload imageHandler={imageHandler} />
-                    <FormTextarea name='description' />
-                    <FormContacts />
-                    <Button type="submit">Добави обява</Button>
-                </form>
-            </FormProvider>
-        </div>
+        loading ? (
+            <Loader loading={loading} color='#1e293b' override={{}} />
+        )
+            :
+            (
+                <div className="w-full flex flex-col items-center" >
+                    <FormProvider {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col flex-initial w-full max-w-[1000px] p-4 gap-6  text-slate-800">
+                            <FormLabel className=" text-slate-800 text-2xl">Какво продаваш</FormLabel>
+                            <div className="flex flex-col gap-6 bg-slate-200 p-4">
+                                <FormInput name='product' type='text' placeholder='Име на обявата' className="bg-white" />
+                                <FormSelect name='category' placeholder='Избери категория' />
+                            </div>
+                            <ImageUpload imageHandler={imageHandler} />
+                            <FormTextarea name='description' />
+                            <FormContacts />
+                            <Button type="submit">Добави обява</Button>
+                        </form>
+                    </FormProvider>
+                </div>
+            )
     )
 }
 
