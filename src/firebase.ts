77d5137b73e0ac7de1f5e5a91e83274e13ed9081
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-06 16:37:44 +0300

    added storage to firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"
+import { getStorage, ref } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,8 +21,10 @@ const app = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(app);
 const firestore = getFirestore(app);
 const firebaseStorage = getStorage(app)
+const storageRef = (imageName: string) => ref(firebaseStorage, imageName);
 export {
     firebaseAuth,
     firestore,
-    firebaseStorage
+    firebaseStorage,
+    storageRef
 }
