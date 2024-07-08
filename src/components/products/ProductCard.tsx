75dabcdd12c552e69a9bb9f324c0beff4f7b6731
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-08 11:31:19 +0300

    implemented products page

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,29 @@
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/ui/card"
+type ProductCardPropsType = {
+    image: string,
+    product: string,
+    price: string,
+}
+
+const ProductCard = ({ image, product, price }: ProductCardPropsType) => {
+    return (
+        <Card className="max-w-[300px] max-h-96 h-full flex flex-col gap-4">
+            <CardHeader>
+                <img src={image} alt="" className="w-[350px] max-h-60 rounded-t-sm aspect-square"/>
+            </CardHeader>
+            <CardContent className="">
+                <CardTitle className="text-black text-base break-words">{product}</CardTitle>
+                <p className="font-bold text-xl text-slate-800 pt-4">{price} лв.</p>
+            </CardContent>
+        </Card>
+
+
+    )
+}
+
+export default ProductCard
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,9 +1,80 @@
+import { useEffect, useState } from "react"
+import ProductCard from "./ProductCard"
+import Aside from "./Aside"
+import { getDocs } from "firebase/firestore"
+import { getAllItemsFromCollection } from "@/firebase"
+import CardPagination from "../common/CardPagination"
+
+interface Product {
+    id: string;
+    images: string[];
+    product: string;
+    price: string;
+    category: string;
+    displayName: string;
+    phoneNumber: string;
+    description: string;
+    email: string;
+}
 
 const Products = () => {
+    const productsPerPage = 12
+    const [products, setProducts] = useState<Product[]>([]);
+    const [startIndex, setStartIndex] = useState(0);
+    const [endIndex, setEndIndex] = useState(productsPerPage)
 
-    
+    async function fetchProducts() {
+        try {
+            const querySnapshot = await getDocs(getAllItemsFromCollection('products'));
+
+            const productsList: Product[] = querySnapshot.docs.map(doc => {
+                const id = doc.id;
+                const data = doc.data()
+                return { ...data, id } as Product
+            });
+            setProducts(productsList);
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const startIndexHandler = (i: number) => {
+        setStartIndex(i)
+    }
+
+    const endIndexHandler = (i: number) => {
+        setEndIndex(i)
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+
     return (
-        <h1 className="">Products page!</h1>
+        <div className="flex flex-col justify-end p-2 w-full xl:px-44 xl:gap-10 xl:flex-row xl:items-start flex-wrap">
+            <aside className="max-w-72 w-full p-4 flex-1">
+                <p className="text-center">Избери категория</p>
+            </aside>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3">
+                {products.slice(startIndex, endIndex).map((p) => <ProductCard
+                    key={p.id}
+                    image={p.images[0]}
+                    product={p.product}
+                    price={p.price}
+                />)}
+            </div>
+            <div>
+                <CardPagination
+                    startIndex={startIndex}
+                    endIndex={endIndex}
+                    startIndexHandler={startIndexHandler}
+                    endIndexHandler={endIndexHandler}
+                    productsPerPage={productsPerPage}
+                    productsLength={products.length} />
+            </div>
+        </div>
     )
 }
 
