da1f9f1f6d32f57d92212ef327e5c38bc50df105
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-09 12:46:47 +0300

    added link with id for each element

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -4,25 +4,27 @@ import {
     CardHeader,
     CardTitle,
 } from "@/ui/card"
+import { Link } from "react-router-dom"
 type ProductCardPropsType = {
     image: string,
     product: string,
     price: string,
+    id: string,
 }
 
-const ProductCard = ({ image, product, price }: ProductCardPropsType) => {
+const ProductCard = ({ image, product, price, id }: ProductCardPropsType) => {
     return (
-        <Card className="max-w-[300px] max-h-96 h-full flex flex-col gap-4">
-            <CardHeader>
-                <img src={image} alt="" className="w-[350px] max-h-60 rounded-t-sm aspect-square"/>
-            </CardHeader>
-            <CardContent className="">
-                <CardTitle className="text-black text-base break-words">{product}</CardTitle>
-                <p className="font-bold text-xl text-slate-800 pt-4">{price} лв.</p>
-            </CardContent>
-        </Card>
-
-
+        <Link to={id} >
+            <Card className="max-w-[300px] max-h-96 h-full flex flex-col gap-4">
+                <CardHeader>
+                    <img src={image} alt="" className="w-[350px] max-h-60 rounded-t-sm aspect-square" />
+                </CardHeader>
+                <CardContent className="">
+                    <CardTitle className="text-black text-base break-words">{product}</CardTitle>
+                    <p className="font-bold text-xl text-slate-800 pt-4">{price} лв.</p>
+                </CardContent>
+            </Card>
+        </Link>
     )
 }
 
