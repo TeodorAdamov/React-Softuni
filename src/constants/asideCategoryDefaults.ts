40a29a0477608ecbb082630ab901579ae8d4b17c
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-09 12:47:10 +0300

    added default categories constant

---
+++ src/constants/asideCategoryDefaults.ts
@@ -0,0 +1,38 @@
+export const DEFAULT_CATEGORIES = [
+    {
+        label: 'Въдици',
+        checked: false
+    },
+    {
+        label: 'Макари',
+        checked: false
+    },
+    {
+        label: 'Примамки',
+        checked: false
+    },
+    {
+        label: 'Куки',
+        checked: false
+    },
+    {
+        label: 'Влакна',
+        checked: false
+    },
+    {
+        label: 'Аксесоари',
+        checked: false
+    },
+    {
+        label: 'Облекло и обувки',
+        checked: false
+    },
+    {
+        label: 'Лодки и мотори',
+        checked: false
+    },
+    {
+        label: 'Други',
+        checked: false
+    }
+]
