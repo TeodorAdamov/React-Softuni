fff4f4a438e4f04368a844ad2ece176ef383a061
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-26 09:28:21 +0300

    added info on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,22 @@
+import { useAuth } from "@/context/authContext"
+import { isUserDefined } from "@/utils/typeGuards";
+import { Link } from "react-router-dom"
+
 const Home = () => {
+    const { user } = useAuth();
     return (
-        <h1 className="">Home page!</h1>
+
+        <div className="flex flex-col max-w-[1100px] p-4 bg-slate-200 rounded-md">
+            <h1>Добре дошли в Fishing Club</h1>
+            <h4>Вашият онлайн пазар за рибарски такъми</h4>
+            <p className="text-2xl pt-2">Fishing Club е мястото, където можете лесно да създавате обяви и да продавате рибарски принадлежности.</p>
+            {!isUserDefined(user) &&
+                <>
+                    <Link to="/register" className="pt-10 text-lg font-bold underline">Започнете сега!</Link>
+                    <p className="text-lg">Регистрирайте се и създайте своята първа обява или разгледайте нашите категории, за да намерите необходимото за вашето следващо риболовно приключение. Присъединете се към Fishing Club и нека заедно уловим големите риби!</p>
+                </>
+            }
+        </div>
     )
 }
 
