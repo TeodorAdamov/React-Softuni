a805495a0b507f4d3ab4b6dacae632d493cd2a37
TeodorAdamov <133758461+TeodorAdamov@users.noreply.github.com>
2024-07-26 09:32:28 +0300

    added a new method to get specific items by email

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, doc } from "firebase/firestore";
+import { getFirestore, collection, doc, query, where } from "firebase/firestore";
 import { getStorage, ref } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,19 +16,23 @@ const firebaseConfig = {
     appId: "1:180878413687:web:cc2f6cacf6a1827f381b72"
 };
 
+const COLLECTION_NAME = 'products'
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(app);
 const firestore = getFirestore(app);
-const getAllItemsFromCollection = (collectionName: string) => collection(firestore, collectionName);
+const getAllItemsFromCollection = () => collection(firestore, COLLECTION_NAME);
 const documentByIdRef = (id: string) => doc(firestore, 'products', id);
 const firebaseStorage = getStorage(app)
 const storageRef = (imageName: string) => ref(firebaseStorage, imageName);
+const specificItemsByEmail = (email: string) => query(collection(firestore, COLLECTION_NAME), where('email', '==', email))
 export {
     firebaseAuth,
     firestore,
     firebaseStorage,
     storageRef,
     getAllItemsFromCollection,
-    documentByIdRef
+    documentByIdRef,
+    specificItemsByEmail
 }
